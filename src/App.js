519992b8568088bb2758a0dcd4fe22c9d9ca959d
graziellyf <132098875+graziellyf@users.noreply.github.com>
2023-06-23 17:17:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,31 @@ function App() {
   const [txtPokemon, alteratxtPokemon ] = React.useState("");
 
   function buscaPokemon(){
-    console.log( txtPokemon )
+
+    if (txtPokemon == "") {
+      buscaTodosPokemons();
+      return;
+    }
+
+
+    axios.get("https://pokeapi.co/api/v2/pokemon/"+txtPokemon)
+    .then( response => {
+      console.log("Requisição bem sucedida!");
+      alteraPokemons ( [ response.data ] );
+    } ) 
+    .catch( response => {
+      alert ("Esse pokemon não existe");
+      console.log("Deu ruim na requisição");
+      console.log(response);
+    })
   }
   
+
   function buscaTodosPokemons(){
     axios.get("https://pokeapi.co/api/v2/pokemon")
-    .then( Response => {
+    .then( response => {
       console.log("Requisição bem sucedida!");
-      alteraPokemons (Response.data.results);
+      alteraPokemons (response.data.results);
     } ) 
     .catch( response => {
       console.log("Deu ruim na requisição");
@@ -42,7 +59,7 @@ function App() {
           pokemons.map( (pokemon, index) =>
             <div>
               <p> { pokemon.name }</p>   
-              <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+(index+1)+".gif"} />
+              <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+( pokemon.id ? pokemon.id : index + 1)+".gif"} />
             </div>
           )
         }
